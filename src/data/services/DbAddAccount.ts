1e02a5fc368d64eb50eaccc3ba69dfbc6b39befb
Jayromberg Lima Santos <jayro@pop-os.localdomain>
2022-11-27 11:31:12 -0300

    feat: Inicio da implementação da camanda de service

---
+++ src/data/services/DbAddAccount.ts
@@ -0,0 +1,21 @@
+import { AccountModel } from "@/domain/entity/account";
+import { AddAccount, AddAccountModel } from "../../../src/domain/use-case/add-account";
+
+export default class DbAddAccount implements AddAccount {
+  private readonly encrypter: Encrypt;
+  private readonly addUserRepo: AddAccount
+
+  constructor(addUserRepo: AddAccount, encrypter: Encrypt) {
+    this.addUserRepo = addUserRepo
+    this.encrypter = encrypter;
+  }
+
+  async add(account: AddAccountModel): Promise<AccountModel> {
+    const { name, email, password  } = account;
+
+    const hash = this.encrypter.encrypt(password)
+
+    const newAccount = await this.addUserRepo.add({ name, email, password: hash });
+    return newAccount;
+  }
+}
