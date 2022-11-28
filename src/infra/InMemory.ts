4103c7ab2cfff7ac5ec801f8927c927ea81b7d2f
Jayromberg Lima Santos <jayro@pop-os.localdomain>
2022-11-28 18:35:25 -0300

    feat: Implementado o getByEmail no InMemoryDb

--- src/infra/InMemory.ts
+++ src/infra/InMemory.ts
@@ -1,16 +1,21 @@
-import { DataBase, DataBaseId } from './protocols/InMemoryDb';
+import { DataBase, columnDb } from './protocols/InMemoryDb';
 
-export default class InMemory<T extends DataBaseId> implements DataBase<T> {
+export default class InMemory<T extends columnDb> implements DataBase<T> {
   private db: Array<T> = []
   private static id: number = 0;
 
   set(value: T): T {
-    this.db.push(value);
+    this.db.push({ id: InMemory.id, ...value });
     return value;
   }
 
   get(id: number): T {
     const [data] = this.db.filter((data) => data.id === id);
+    return data;
+  }
+
+  getByEmail(email: string): T {
+    const [data] = this.db.filter((data) => data.email === email);
     return data;
   }
 
--- src/infra/protocols/InMemoryDb.ts
+++ src/infra/protocols/InMemoryDb.ts
@@ -1,9 +1,11 @@
-export interface DataBaseId {
+export interface columnDb {
   id: number;
+  email: string;
 }
 
 export interface DataBase<T> {
   set(value: T): T;
   get(id: number): T | undefined;
+  getByEmail(email: string): T | null;
   getAll(): Array<T> | [];
 }
